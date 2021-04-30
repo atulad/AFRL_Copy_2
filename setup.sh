11e18f06ef93fb4299f794de6b0715c91c343cc8
Josiah Coad <josiah@coad.net>
2021-04-30 06:38:46 -0500

    removing echo from setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # this file is used by ec2_cluster.sh
-idx=$2
-mkdir -p results/tboard
 sudo pkill PackageKit # else, this program holds the yum lock
+sleep 10 # wait for yum lock
 sudo yum install gcc-c++ python3-devel -y  > /dev/null
 pip3 install --user -r requirements.txt > /dev/null
-echo packages installed on $idx
