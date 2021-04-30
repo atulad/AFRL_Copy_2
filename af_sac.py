134cb83a49ed2a6870249a74a90e5aaa77bb17c4
Josiah Coad <josiah@coad.net>
2021-04-30 06:38:16 -0500

    updates to af_sec (saving delta in tb filename)

--- af_sac.py
+++ af_sac.py
@@ -11,10 +11,12 @@ rand_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
 
 env = gym.make('Pendulum-v0')
 
+delta = 0.95
+
 model = AF_SAC(
     'MlpPolicy',
     env=gym.make('LunarLanderContinuous-v2'),
-    delta=2,
+    delta=delta,
     forecast_horizon=8,
     dynamics_layers=[32, 32],
     dynamics_lr=1e-4,
@@ -27,6 +29,5 @@ model = AF_SAC(
 
 model.learn(
     total_timesteps=200_000,
-    tb_log_name=f'P_AF_SAC_{rand_id}'
+    tb_log_name=f'P_AF_SAC_delta={delta}_{rand_id}'
 )
-
